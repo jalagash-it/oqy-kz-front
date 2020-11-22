4ad8807d60615a7f5f912cfcfc4e69ece8dd5280
karnurmax <karnurmax@mail.ru>
2020-11-22 22:21:00 +0500

    epmty navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,68 @@
 <template>
   <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">ОҚЫ</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Жоба жайлы</b-nav-item>
+          <b-nav-item href="#">Ашық аферта</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Іздеу"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Іздеу</b-button
+            >
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Тіл" right>
+            <b-dropdown-item href="#">KK</b-dropdown-item>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown right v-if="user != null">
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Профиль</b-dropdown-item>
+            <b-dropdown-item href="#">Шығу</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown right v-else>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>Қонақ</em>
+            </template>
+            <b-dropdown-item href="#">Кіру</b-dropdown-item>
+            <b-dropdown-item href="#">Тіркелу</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <Nuxt />
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      user: null,
+    };
+  },
+};
+</script>
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
